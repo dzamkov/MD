e615dbf0761e283c30152f0c9ccc958223bfe91b
dzamkov <dzamkov@xiatek.org>
2012-01-02 14:46:27 -0500

    Bit of tweaking on Frame (mostly comments)

--- Source/DSP/Frame.fs
+++ Source/DSP/Frame.fs
@@ -6,10 +6,7 @@ open MD
 open MD.Util
 
 /// An collection of convolution kernels that localize a discrete signal in time and frequency. Kernel's are
-/// ordered such that a kernel with a higher index has higher frequency content. A frame must be invertible 
-/// such that, after applying all convolution kernels, it is possible to reconstruct the original signal
-/// based on the convolved signals. In order to be invertible, the sum of all spectral kernels should have a support
-/// covering the entire spectrum.
+/// ordered such that a kernel with a higher index has higher frequency content.
 [<AbstractClass>]
 type Frame (size : int) =
 
@@ -28,7 +25,7 @@ type Frame (size : int) =
             else getMax (max curMax (this.GetKernelSize index)) (index + 1, count - 1)
         getMax 0 (index, count)
 
-    /// Writes the temporal kernel with the given index to the given cyclic buffer. Note that the kernel will be 
+    /// Writes the temporal kernel with the given index to the given cyclical buffer. Note that the kernel will be 
     /// centered on sample 0, since that makes it more useful for convolution. If the buffer has extra space,
     /// the extra center samples will be zero-padded. 
     abstract GetTemporalKernel : int -> Buffer<Complex> * int -> unit
@@ -49,8 +46,11 @@ type Frame (size : int) =
         DFT.computeComplex tempBuffer buffer size
         unpinTemp ()
 
-/// A frame that contains linearly-spaced spectral kernels based on window functions.
-type LinearFrame (window : Window, windowSize : float, size : int) =
+/// A frame that contains linearly-spaced spectral kernels based on window functions. The "full" parameter determines
+/// what portion of the spectrum the frame covers. A value of true indicates that the frame covers the entire spectrum and is
+/// invertible for all signals. A value of false indicates that the frame covers the first half of the spectrum and is invertible
+/// for real signals.
+type LinearFrame (window : Window, windowSize : float, size : int, full : bool) =
     inherit Frame (size)
     let kernelSize = round (int (ceil windowSize)) 2 + 1
 
@@ -62,7 +62,7 @@ type LinearFrame (window : Window, windowSize : float, size : int) =
         let leadingSize = kernelSize / 2
         let trailingSize = kernelSize - leadingSize
         let paddingSize = size - kernelSize
-        let freq = float index / float this.Size
+        let freq = (if full then 1.0 else 0.5) * float index / float this.Size
         let mult = 1.0 / windowSize
 
         // Trailing half
